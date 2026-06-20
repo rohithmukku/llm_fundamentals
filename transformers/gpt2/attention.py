resolve bug of redundant computation
Signed-off-by: Rohith Mukku <rohithmukku@gmail.com>

transformers/gpt2/attention.py
@@ -38,7 +38,7 @@ class CausalSelfAttention(nn.Module):
             self.register_buffer("causal_mask", torch.tril(torch.ones(self.max_seq_len, self.max_seq_len)).view(1, 1, self.max_seq_len, self.max_seq_len))
         
 
-    def forward(self, x, past_kv=None):
+    def forward(self, x, past_kv=None, return_kv_cache=False):
         # x: (B, T, D)
         B, T, D = x.size()
 
@@ -86,7 +86,7 @@ class CausalSelfAttention(nn.Module):
             o = o.transpose(1, 2).contiguous().view(B, 1, D)
         o = self.resid_dropout(self.projection(o))
 
-        return o, (k, v)
+        return o, (k, v) if return_kv_cache else None
 
 def test_attention():
     config = ModelConfig(n_embed=128, n_heads=4, max_seq_len=32, dropout=0.0, bias=True, use_torch_dot_product=False)

transformers/gpt2/block.py
@@ -11,8 +11,8 @@ class Block(nn.Module):
         self.ln_2 = nn.LayerNorm(config.n_embed, bias=config.bias)
         self.mlp = MLP(config)
 
-    def forward(self, x, past_kv=None):
-        o, kv = self.attention(self.ln_1(x), past_kv)
+    def forward(self, x, past_kv=None, return_kv_cache=False):
+        o, kv = self.attention(self.ln_1(x), past_kv, return_kv_cache)
         x = x + o
         x = x + self.mlp(self.ln_2(x))
 

transformers/gpt2/gpt2.py
@@ -38,7 +38,7 @@ class GPT2(nn.Module):
 
         for i, block in enumerate(self.blocks):
             past_kv = past_kvs[i] if past_kvs is not None else None
-            x, new_kv = block(x, past_kv)
+            x, new_kv = block(x, past_kv, return_kv_cache)
             new_kvs.append(new_kv)
         
         x = self.ln(x)